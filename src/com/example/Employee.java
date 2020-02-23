65a5141f2012aa8479e16bd5a11fbb84544afcd5
Kandakatlasahithi <kandakatlasahithi@gmail.com>
2020-02-23 13:28:59 +0530

    second commit

--- src/com/example/Employee.java
+++ src/com/example/Employee.java
@@ -1,10 +1,12 @@
 package com.example;
 
 public class Employee {
+	
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+System.out.println("first push");
 	}
 
 }
